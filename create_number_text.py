feadcec2ef361a7689cd4377648a58420850f840
Pham Son <sp@outlook.com.vn>
2021-06-29 10:45:17 +0700

    Add script generating vietnamese number string for tranining

---
+++ create_number_text.py
@@ -0,0 +1,12 @@
+from tacotron2.text.vi_number_and_units import normalize_vi
+from random import randrange
+
+
+print("Start generate vietnamese number strings")
+
+with open("trainning_data/vietnamese_number.txt", 'w', encoding='utf-8') as f: 
+    for i in range(20000):
+        n = randrange(1000000000, 2000000000)
+        f.write(normalize_vi(str(n)) + '\n')
+
+print("done!")
