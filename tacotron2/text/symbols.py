a8e34e17af02cf18f9adc9b55d37b13cd0839ed4
Pham Son <sp@outlook.com.vn>
2021-06-29 08:56:42 +0700

    Modified symbols

--- tacotron2/text/symbols.py
+++ tacotron2/text/symbols.py
@@ -4,7 +4,7 @@
 Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
-from tacotron2.text import cmudict
+#from tacotron2.text import cmudict
 
 _pad        = '_'
 _punctuation = '!\'(),.:;? '
@@ -12,7 +12,9 @@ _special = '-'
 _letters = 'aáảàãạâấẩầẫậăắẳằẵặbcdđeéẻèẽẹêếểềễệfghiíỉìĩịjklmnoóỏòõọôốổồỗộơớởờỡợpqrstuúủùũụưứửừữựvwxyýỷỳỹỵz'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
+#_arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+#symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+
+symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters)
